0e1b0cb46383c1bbb29c385f1e07b441df348e77
cachaucanes23 <cachaucanes23@gmail.com>
2020-05-07 21:34:07 -0500

    Covid

---
+++ precache-manifest.aef38750a52b0bcf2186157003b8e4b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235724f810f601793bcd9f526bbc6048",
+    "url": "/Softimus-covid-19/index.html"
+  },
+  {
+    "revision": "3ae2f8d6726cadca99e8",
+    "url": "/Softimus-covid-19/static/css/main.97c3cbaa.chunk.css"
+  },
+  {
+    "revision": "f24cb02ff9a8db8dcff6",
+    "url": "/Softimus-covid-19/static/js/2.fe9d6cf5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Softimus-covid-19/static/js/2.fe9d6cf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ae2f8d6726cadca99e8",
+    "url": "/Softimus-covid-19/static/js/main.92f2ddb9.chunk.js"
+  },
+  {
+    "revision": "71381785389c3e589bba",
+    "url": "/Softimus-covid-19/static/js/runtime-main.10f274a0.js"
+  }
+]);
